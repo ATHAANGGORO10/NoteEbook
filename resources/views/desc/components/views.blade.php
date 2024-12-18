a6b83df6f8f155b261bb606443b379f409d39e32
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2024-12-18 13:27:27 +0700

    testing

--- resources/views/desc/components/views.blade.php
+++ resources/views/desc/components/views.blade.php
@@ -1,5 +1,14 @@
 @extends('app.root')
 
 @section('content')
-  <a href="{{ route('edited', $data->id) }}">edit data</a>
+    <section>
+        @if ($data->banner)
+            <img class="max-w-full max-h-56" src="{{ asset('banner/' . $data->banner) }}">
+        @else
+            <img class="max-w-full max-h-56" src="{{ asset('asset/asset-dashboard/asset-2.svg') }}">
+        @endif
+          <aside class="">
+            <a href="{{ url($data->url) }}" target="_blank" rel="noopener noreferrer" type="button">Baca</a>
+        </aside>
+    </section>
 @endsection
